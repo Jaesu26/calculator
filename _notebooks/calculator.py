2d073e86fd271645f7b12c14904acf6e2ad4c414
gkswotn12345 <84672164+gkswotn12345@users.noreply.github.com>
2021-07-19 00:33:32 +0900

    Create calculator.py
    Change file name

---
+++ _notebooks/calculator.py
@@ -0,0 +1,12 @@
+## 파이썬의 tkinter를 통해 계산기를 만들어보자
+## tkinter import 하기
+import tkinter as tk
+
+## 윈도우창 생성
+window = tk.Tk()
+
+## 윈도우 제목 설정
+window.title('계산기')
+
+## 종료할 때까지 윈도우창 띄우기
+window.mainloop()
